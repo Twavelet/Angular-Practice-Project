0e45100e21652acd735bb58b2478b63a65fa6922
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-11-30 13:08:51 -0500

    Added functionality for POST requests

--- Angular-crash-course/Crash-Course/src/app/components/tasks/tasks.component.ts
+++ Angular-crash-course/Crash-Course/src/app/components/tasks/tasks.component.ts
@@ -33,4 +33,8 @@ export class TasksComponent {
       .updateTaskReminder(task)
       .subscribe();
   }
+
+  addTask(task: Task){
+    this.taskService.addTask(task).subscribe((task) => (this.tasks.push(task)));
+  }
 }
